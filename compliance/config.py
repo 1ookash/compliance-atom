76570696855f5ffe9da50fbddfa7a83d56f708d6
Lukash Vladislav <vlukash@sdvor.com>
2024-11-10 04:24:45 +0300

    compliance, fix qwen_main.py

--- compliance/config.py
+++ compliance/config.py
@@ -9,6 +9,7 @@ env.read_env()
 @dataclass(config=pydantic.ConfigDict(frozen=True, strict=True, extra='forbid'))
 class Config:
     llm_url: str = env.str('LLM_URL')
+    llm_absolute_fpath: str = env.str('LLM_ABSOLUTE_FPATH')
     db_fpath: str = env.str('DB_FPATH')
 
 
